a0cfa27d905c2f38bb9ffe10506d9a3c2201b53d
Yinka Jimoh <111042679+j2dmail@users.noreply.github.com>
2023-02-17 10:03:42 -0800

    fortune teller

---
+++ tellFortune.js
@@ -0,0 +1,8 @@
+function tellFortune(noOfChildren, partnerName, location, jobTitle) {
+    return "You will be an " + jobTitle + " in " + location + " and married to " + partnerName + " with " + noOfChildren + " kids.";
+  }
+  
+  console.log(tellFortune(2, "Qudrah", "Helsinki", "Senior Software Engineer"));
+  console.log(tellFortune(3, "Olajumoke", "Lagos", "Senior Dev Engineer"));
+  console.log(tellFortune(4, "Wemimo", "London", "Senior Actuary"));
+  console.log(tellFortune(2, "Eyitayo", "Ibadan", "Senior Engineer"));
